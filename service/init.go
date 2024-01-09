8d064fbdba05ee80c8335275e3d446d3ca155f9b
Peter Bwire <bwire517@gmail.com>
2024-01-09 08:57:09 +0300

    improve token enrichment webhook

--- service/init.go
+++ service/init.go
@@ -103,7 +103,7 @@ func NewAuthRouterV1(service *frame.Service,
 	holder.addHandler(sRouter, handlers.SetPasswordEndpoint, "/password", "SetPasswordEndpoint", "GET")
 	holder.addHandler(sRouter, handlers.ForgotEndpoint, "/forgot", "ForgotEndpoint", "GET")
 
-	holder.addHandler(webhookRouter, handlers.TokenEnrichmentEndpoint, "/enrich/token", "WebhookTokenEnrichmentEndpoint", "POST")
+	holder.addHandler(webhookRouter, handlers.TokenEnrichmentEndpoint, "/enrich/{tokenType}", "WebhookTokenEnrichmentEndpoint", "POST")
 	holder.addHandler(webhookRouter, handlers.CentrifugoProxySubscribeEndpoint, "/proxy/subscription", "CentrifugoProxySubscribeEndpoint", "POST")
 
 	holder.addHandler(authRouter, handlers.CreateAPIKeyEndpoint, "/key", "CreateAPIKeyEndpoint", "PUT")
