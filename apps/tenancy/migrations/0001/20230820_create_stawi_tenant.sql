31edeefcb6f4bdbdbd430b7153e681fa1446ec8c
Peter Bwire <bwire517@gmail.com>
2025-11-30 14:53:32 +0300

    don't set client secret for public clients

--- apps/tenancy/migrations/0001/20230820_create_stawi_tenant.sql
+++ apps/tenancy/migrations/0001/20230820_create_stawi_tenant.sql
@@ -1,7 +1,7 @@
 -- Default base partition
 INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bsv0s0hijjg02z5lbjg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'Stawi', 'Default base tenant for stawi');
-INSERT INTO partitions (id, tenant_id, partition_id, name, description, client_secret, properties)
+INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
     VALUES('9bsv0s0hijjg02qk7l1g', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg',
-           'Stawi', 'Default stawi partition to serve the masses', 'Sec_oZemEHfunPu6r4AJr2',
+           'Stawi', 'Default stawi partition to serve the masses',
            '{"scope": "openid offline_access profile contact", "audience": ["service_chat_drone","service_chat_gateway", "service_devices", "service_profile", "service_files"], "logo_uri": "https://static.stawi.im/logo.png", "redirect_uris": ["https://app.stawi.im/sso/callback"], "post_logout_redirect_uris": ["https://app.stawi.im/sso/logout"]}');
 
