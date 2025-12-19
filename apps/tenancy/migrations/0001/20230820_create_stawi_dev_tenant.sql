f9e06df6cac883d3bb3860e9a24c6de7ee38b803
Peter Bwire <bwire517@gmail.com>
2025-12-19 21:21:18 +0300

    streamline migrations for authentication tenancy

--- apps/tenancy/migrations/0001/20230820_create_stawi_dev_tenant.sql
+++ apps/tenancy/migrations/0001/20230820_create_stawi_dev_tenant.sql
@@ -1,7 +1,7 @@
 -- Default base partition
 INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bsv0s0hijjg09bzz6dg', '9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qhg', 'Stawi Development', 'Default base tenant for testing and building stawi');
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
-    VALUES('9bsv0s0hijjg02qks6i0', '9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qhg',
+    VALUES('9bsv0s0hijjg02qks6i0', '9bsv0s0hijjg09bzz6dg', '9bsv0s0hijjg02qks6i0',
            'Stawi Development', 'Default Stawi development partition',
            '{"scope": "openid offline offline_access profile contact", "audience": ["service_chat_drone","service_chat_gateway", "service_devices", "service_profile", "service_files"], "logo_uri": "https://static.stawi.im/logo.png", "redirect_uris": ["https://app-dev.stawi.im/sso/redirect", "com.antinvestor.chat://sso/redirect", "https://localhost:5170/sso/redirect"], "post_logout_redirect_uris": ["https://app-dev.stawi.im/sso/logout", "http://localhost:8008/sso/logout", "https://localhost:5170/sso/logout"]}');
 
--- apps/tenancy/migrations/0001/20230820_create_stawi_tenant.sql
+++ apps/tenancy/migrations/0001/20230820_create_stawi_tenant.sql
@@ -1,7 +1,7 @@
 -- Default base partition
 INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bsv0s0hijjg02z5lbjg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'Stawi', 'Default base tenant for stawi');
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
-    VALUES('9bsv0s0hijjg02qk7l1g', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg',
+    VALUES('9bsv0s0hijjg02qk7l1g', '9bsv0s0hijjg02z5lbjg', '9bsv0s0hijjg02qk7l1g',
            'Stawi', 'Default stawi partition to serve the masses',
            '{"scope": "openid offline offline_access profile contact", "audience": ["service_chat_drone","service_chat_gateway", "service_devices", "service_profile", "service_files"], "logo_uri": "https://static.stawi.im/logo.png", "redirect_uris": ["https://app.stawi.im/sso/redirect", "com.antinvestor.chat://sso/redirect", "http://localhost:5170/sso/redirect"], "post_logout_redirect_uris": ["https://app.stawi.im/sso/logout"]}');
 
