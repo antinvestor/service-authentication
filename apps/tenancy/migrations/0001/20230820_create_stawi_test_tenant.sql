f1b23e98a04b6dba389de600c52aba23554de7fa
Peter Bwire <bwire517@gmail.com>
2026-04-02 09:13:00 +0300

    chore: add license headers and update generated protobuf artifacts
    Add Apache 2.0 license headers to all source files missing them.
    Update generated Dart protobuf descriptors and package tooling.

--- apps/tenancy/migrations/0001/20230820_create_stawi_test_tenant.sql
+++ apps/tenancy/migrations/0001/20230820_create_stawi_test_tenant.sql
@@ -1,3 +1,17 @@
+--- Copyright 2023-2026 Ant Investor Ltd
+---
+--- Licensed under the Apache License, Version 2.0 (the "License");
+--- you may not use this file except in compliance with the License.
+--- You may obtain a copy of the License at
+---
+---      http://www.apache.org/licenses/LICENSE-2.0
+---
+--- Unless required by applicable law or agreed to in writing, software
+--- distributed under the License is distributed on an "AS IS" BASIS,
+--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--- See the License for the specific language governing permissions and
+--- limitations under the License.
+
 -- Default base partition
 INSERT INTO tenants (id, tenant_id, partition_id, name, description, environment) VALUES('9bsv0s0hijjg09bzz6dg', '9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qhg', 'Stawi Development', 'Default base tenant for testing and building stawi', 'staging');
 INSERT INTO partitions (id, tenant_id, partition_id, parent_id, name, description, allow_auto_access, properties)
