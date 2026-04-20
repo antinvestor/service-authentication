71adf5ace2ac9740cf4adcd2e69404857d74d6b5
Peter Bwire <bwire517@gmail.com>
2026-04-20 09:14:00 +0300

    migration(tenancy): v2 service_savings

---
+++ apps/tenancy/migrations/0001/20260420_service_service_savings.sql
@@ -0,0 +1,40 @@
+-- Copyright 2023-2026 Ant Investor Ltd
+-- Service account: service-savings
+-- Savings account management. Handles savings products, accounts,
+-- deposits, withdrawals, and interest calculations. Calls
+-- operations for transfer execution.
+
+INSERT INTO clients (
+    id, tenant_id, partition_id, name, client_id, client_secret,
+    type, grant_types, scopes, audiences,
+    token_endpoint_auth_method, service_account_id, properties
+) VALUES (
+    'c2f4j7au6s7f91uqnq5g',
+    'c2f4j7au6s7f91uqnojg',
+    'c2f4j7au6s7f91uqnokg',
+    'sa-service_savings',
+    'service-savings',
+    '',
+    'internal',
+    '{"types": ["client_credentials"]}',
+    'system_int openid',
+    '{"service_audit":["*"],"service_identity":["*"],"service_operations":["*"]}',
+    'private_key_jwt',
+    'c2f4j7au6s7f91uqnq6g',
+    '{"jwks_uri": "https://oauth2.stawi.org/.well-known/jwks.json"}'
+) ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO service_accounts (
+    id, tenant_id, partition_id, profile_id,
+    client_id, client_ref, type, audiences, properties
+) VALUES (
+    'c2f4j7au6s7f91uqnq6g',
+    'c2f4j7au6s7f91uqnojg',
+    'c2f4j7au6s7f91uqnokg',
+    'd75qclkpf2t1uum8ijfg',
+    'service-savings',
+    'c2f4j7au6s7f91uqnq5g',
+    'internal',
+    '{"service_audit":["*"],"service_identity":["*"],"service_operations":["*"]}',
+    '{}'
+) ON CONFLICT (id) DO NOTHING;
