b740a219005c716088db08457a5dbac61c3c6c3c
Peter Bwire <bwire517@gmail.com>
2026-04-30 18:53:28 +0300

    set the redirect uri for stawi chat appropriately

--- apps/tenancy/migrations/0001/20260420_partition_stawi_chat.sql
+++ apps/tenancy/migrations/0001/20260420_partition_stawi_chat.sql
@@ -11,7 +11,7 @@ ON CONFLICT (id) DO NOTHING;
 INSERT INTO partitions (id, tenant_id, partition_id, parent_id, name, description, allow_auto_access, properties)
 VALUES ('9bsv0s0hijjg02qk7l1g','9bsv0s0hijjg02z5lbjg','9bsv0s0hijjg02qk7l1g','c2f4j7au6s7f91uqnokg',
         'Stawi','Default stawi partition to serve the masses',true,
-        '{"default_role":"user","allow_auto_access":true,"support_contacts":{"msisdn":"+256757546244","email":"info@stawi.im"}}')
+        '{"default_role":"user","allow_auto_access":true,"support_contacts":{"msisdn":"+256757546244","email":"chat@stawi.org"}}')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO partition_roles (id, created_at, modified_at, version, tenant_id, partition_id, name, is_default, properties) VALUES
@@ -35,9 +35,9 @@ INSERT INTO clients (
     '{"types": ["code"]}',
     'openid offline_access profile',
     '{"service_chat":["*"],"service_device":["*"],"service_file":["*"],"service_geolocation":["*"],"service_profile":["*"]}',
-    '{"uris":["https://app.stawi.im/sso/redirect","com.antinvestor.chat://sso/redirect","http://localhost:5170/sso/redirect","https://accounts.stawi.org/_internal/fedcm-callback"]}',
-    'https://static.stawi.im/logo.png',
-    '{"uris":["https://app.stawi.im/sso/logout"]}',
+    '{"uris":["https://chat.stawi.org/sso/redirect","org.stawi.chat://sso/redirect","http://localhost:5170/sso/redirect","https://accounts.stawi.org/_internal/fedcm-callback"]}',
+    'https://static.stawi.org/logo.png',
+    '{"uris":["https://chat.stawi.org/sso/logout"]}',
     'none'
 ) ON CONFLICT (id) DO NOTHING;
 
@@ -50,7 +50,7 @@ ON CONFLICT (id) DO NOTHING;
 INSERT INTO partitions (id, tenant_id, partition_id, parent_id, name, description, allow_auto_access, properties)
 VALUES ('9bsv0s0hijjg02qks6i0','9bsv0s0hijjg09bzz6dg','9bsv0s0hijjg02qks6i0','c2f4j7au6s7f91uqnokg',
         'Stawi Development','Default Stawi development partition',true,
-        '{"default_role":"user","allow_auto_access":true,"support_contacts":{"msisdn":"+256757546244","email":"info@stawi.im"}}')
+        '{"default_role":"user","allow_auto_access":true,"support_contacts":{"msisdn":"+256757546244","email":"info@stawi.org"}}')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO partition_roles (id, created_at, modified_at, version, tenant_id, partition_id, name, is_default, properties) VALUES
@@ -74,8 +74,8 @@ INSERT INTO clients (
     '{"types": ["code"]}',
     'openid offline_access profile',
     '{"service_chat":["*"],"service_device":["*"],"service_file":["*"],"service_geolocation":["*"],"service_profile":["*"]}',
-    '{"uris":["https://app-dev.stawi.im/sso/redirect","com.antinvestor.chat://sso/redirect","https://localhost:5170/sso/redirect","https://accounts.stawi.org/_internal/fedcm-callback"]}',
-    'https://static.stawi.im/logo.png',
-    '{"uris":["https://app-dev.stawi.im/sso/logout","com.antinvestor.chat://sso/logout","https://localhost:5170/sso/logout"]}',
+    '{"uris":["https://chat-dev.stawi.org/sso/redirect","org.stawi.chat://sso/redirect","https://localhost:5170/sso/redirect","https://accounts.stawi.org/_internal/fedcm-callback"]}',
+    'https://static.stawi.org/logo.png',
+    '{"uris":["https://chat-dev.stawi.org/sso/logout","org.stawi.chat://sso/logout","https://localhost:5170/sso/logout"]}',
     'none'
 ) ON CONFLICT (id) DO NOTHING;
